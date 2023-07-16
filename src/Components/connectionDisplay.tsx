46d7e298c625cfa667dcb47c4754a01365d96ffa
Vincent Vermeire <vvermeire55@gmail.com>
2023-07-16 08:46:18 +0200

    Now shows connection color and tooltip

--- src/Components/connectionDisplay.tsx
+++ src/Components/connectionDisplay.tsx
@@ -1,4 +1,6 @@
 import { HubConnectionState } from "@microsoft/signalr";
+import { useRef } from "react";
+import { Tooltip } from "react-tooltip";
 
 type Props = {
     connectionState: HubConnectionState
@@ -6,9 +8,52 @@ type Props = {
 
 export default function ConnectionDisplay({ connectionState }: Props) {
 
+    const getConnectionColor = (connectionState: HubConnectionState) => {
+        switch (connectionState) {
+            case HubConnectionState.Connected:
+                return "green";
+            case HubConnectionState.Connecting:
+                return "orange";
+            case HubConnectionState.Disconnected:
+                return "red";
+            case HubConnectionState.Disconnecting:
+                return "orange";
+            default:
+                return "black";
+        }
+    }
+
+    const getConnectionStateTooltipContent = (connectionState: HubConnectionState) => {
+        switch (connectionState) {
+            case HubConnectionState.Connected:
+                return "Connected to tracker server";
+            case HubConnectionState.Connecting:
+                return "Connecting to tracker server";
+            case HubConnectionState.Disconnected:
+                return "Disconnected from tracker server";
+            case HubConnectionState.Disconnecting:
+                return "Disconnecting from tracker server";
+            default:
+                return "Unknown connection state";
+        }
+    }
+
+
+    const connectionColor = getConnectionColor(connectionState);
+    const connectionStateElementRef = useRef<HTMLDivElement>(null);
+    if (connectionStateElementRef.current) {
+        connectionStateElementRef.current.style.backgroundColor = connectionColor;
+    }
+
     return (
-        <div>
-            <p>Connection: {connectionState}</p>
-        </div>
-    )   
+        <>
+            <div 
+                ref={connectionStateElementRef} 
+                className="w-4 h-4 rounded-full"
+                data-tooltip-id="connectionStateTooltip"
+                data-tooltip-content={getConnectionStateTooltipContent(connectionState)}
+            />
+            <Tooltip id="connectionStateTooltip" />
+        </>
+    )
 }
