12137855fd3b457b8004a2900002425be15615be
Vincent Vermeire <vvermeire55@gmail.com>
2023-07-19 15:38:06 +0200

    System now shows list of computer displays

--- src/Components/System.tsx
+++ src/Components/System.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useComputers } from "../Hooks/useComputers";
 import useSystem from "../Hooks/useSystem";
+import ComputerDisplay from "./ComputerDisplay";
 
 export default function System() {
     const params = useParams();
@@ -12,21 +13,17 @@ export default function System() {
     if (systemLoading) return <div>Loading system...</div>
     if (computersLoading) return <div>Loading computers...</div>
 
-    if (!system) return <div>System not found</div>
+    if (!system) return <div>Code 404: System not found</div>
 
     return (
-        <div>
+        <div id="system">
             <h1>{system.displayName}</h1>
             <h2>Computers</h2>
-            <ul>
-                {computers.map(computer => {
-                    return (
-                        <li key={computer.id}>
-                            {computer.label}
-                        </li>
-                    )
-                })}
-            </ul>
+            <div className="">
+                {computers.map(computer => 
+                    <ComputerDisplay key={computer.id} computerId={computer.id} />
+                )}
+            </div>
         </div>
     )
 }
