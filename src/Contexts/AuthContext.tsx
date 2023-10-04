14161faea916cc51221fa94a3c49d83c03d6f373
Vincent Vermeire <vvermeire55@gmail.com>
2023-10-04 19:49:59 +0200

    added redirect when not authenticated

--- src/Contexts/AuthContext.tsx
+++ src/Contexts/AuthContext.tsx
@@ -1,10 +1,18 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { AuthState, useAuth } from "../Hooks/useAuth";
 
 export const authContext = createContext<AuthState>({ isSignedIn: false, user: null, pending: true, isMCSPlayer: false});
 
 export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
     const { pending, isSignedIn, user, isMCSPlayer } : AuthState = useAuth();
+
+    //redirect user to login if not signed in
+    useEffect(() => {
+        if (!isSignedIn && !pending) {
+            window.location.href = import.meta.env.VITE_LOGIN_URL + "?redirect=" + window.location.href;
+        }
+    }, [isSignedIn, pending]);
+
     return (
         <authContext.Provider value={{ isSignedIn, user, pending, isMCSPlayer } as AuthState}>
             {children}
