0590475b7aa5817c6bfda952b46ac82524459710
Vincent Vermeire <vvermeire55@gmail.com>
2023-10-05 20:38:27 +0200

    Added comment

--- src/Hooks/useServices.ts
+++ src/Hooks/useServices.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Service } from "../Types/Service";
 
+// NOT IMPLEMENTED YET
+
 export default function useServices() {
     const [services, setServices] = useState<Service[]>([]);
     const [servicesLoading, setServicesLoading] = useState<boolean>(true);
